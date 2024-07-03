fd4975b728252e5676ecc6728ff3ba06c37ec331
GerardZhou <dorey1200@gmail.com>
2024-07-02 21:23:17 -0500

    upd

---
+++ todoapp/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Todo from "./components/Todo";
+import TodoItem from "./components/TodoItem";
+
+function App() {
+  return <Todo />;
+}
+
+export default App;
---
+++ todoapp/src/components/Todo.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import TodoItem from "./TodoItem.jsx";
+export default function Todo() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTodos([...todos, todo]);
+    // console.log(todos);
+    setTodo("");
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={(e) => setTodo(e.target.value)}
+          value={todo}
+          type="text"
+        ></input>
+        <button type="submit">Add</button>
+      </form>
+      {todos.map((item) => (
+        <TodoItem key={item} item={item} />
+      ))}
+    </div>
+  );
+}
