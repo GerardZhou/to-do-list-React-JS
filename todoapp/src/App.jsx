5f6b9831a3912ad21f12c11a3a12d151b7415847
GerardZhou <dorey1200@gmail.com>
2024-07-03 16:39:28 -0500

    finished

--- todoapp/src/App.jsx
+++ todoapp/src/App.jsx
@@ -3,9 +3,15 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import Todo from "./components/Todo";
 import TodoItem from "./components/TodoItem";
-
+import Header from "./components/Header";
+import "./App.css";
 function App() {
-  return <Todo />;
+  return (
+    <div>
+      <Header />
+      <Todo />
+    </div>
+  );
 }
 
 export default App;
--- todoapp/src/components/TodoItem.jsx
+++ todoapp/src/components/TodoItem.jsx
@@ -1,3 +1,35 @@
-export default function TodoItem({ item }) {
-  return <h4>{item}</h4>;
+import styles from "./todoitem.module.css";
+export default function TodoItem({ item, todos, setTodos }) {
+  function handleDelete(item) {
+    // console.log(item, " deleted");
+    setTodos(todos.filter((todo) => todo !== item));
+  }
+  function handleClick(itemName) {
+    // console.log(itemName, " Item clicked");
+    const newArray = todos.map((todo) =>
+      todo.name === itemName ? { ...todo, done: !todo.done } : todo
+    );
+    setTodos(newArray);
+    console.log(todos);
+  }
+  const completedClass = item.done ? styles.completed : "";
+  return (
+    <div className={styles.item}>
+      {" "}
+      <div className={styles.itemName}>
+        <span className={completedClass} onClick={() => handleClick(item.name)}>
+          {item.name}
+        </span>
+        <span>
+          <button
+            onClick={() => handleDelete(item)}
+            className={styles.deleteButton}
+          >
+            x
+          </button>
+        </span>
+      </div>
+      <hr className={styles.line}></hr>
+    </div>
+  );
 }
